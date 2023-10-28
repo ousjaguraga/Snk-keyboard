b2a7942d3bec15451ff63950f223fdf6926d3a0e
ousjaguraga <ousjaguraga19@gmail.com>
2023-10-28 14:07:39 +0100

    Initial Commit

---
+++ Wallaha/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Wallaha
+//
+//  Created by Ovenger on 28/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Wallaha/WallahaApp.swift
@@ -0,0 +1,17 @@
+//
+//  WallahaApp.swift
+//  Wallaha
+//
+//  Created by Ovenger on 28/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct WallahaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
